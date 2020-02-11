98638d7d4e78300b42dd1cd1d46855d51618a877
campbell996 <60759285+campbell996@users.noreply.github.com>
2020-02-11 12:46:58 +1100

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,20 @@
+Find...    class CfgExileArsenal
+Add....	   #include "traders\7IN1\ItemList7IN1.hpp"
+	
+	
+	
+	
+Find...	   class CfgTraderCategories
+Add....    #include "traders\7IN1\TraderCategories7IN1.hpp"
+
+	
+	
+	
+Find...    class CfgTraders
+Find...    class Exile_Trader_Vehicle
+Add....	   "HMCSRobots",
+     	   "WalkersA2Cars"
+		
+Find...    class Exile_Trader_Aircraft
+Add....	   "SABChoppers",
+           "SABPlanes"
